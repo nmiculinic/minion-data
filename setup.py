823371cd5b3873f9fb503524acf665ad9fc4dd82
Neven Miculinic <neven.miculinic@gmail.com>
2018-04-29 14:23:44 +0200

    Added bunch of stuff from minion-basecaller

--- setup.py
+++ setup.py
@@ -5,18 +5,22 @@ from setuptools import setup, find_packages
 # from Cython.Build import cythonize
 
 install_requires=[
+    'pysam>=0.14.0',
+    'tqdm>=4.23.0',
+    'numpy>=1.14.0',
+    'pandas>=0.22.0',
 ]
 
 setup(
-    name="minino_data",
+    name="minion_data",
     version='0.1',
     description='',
     url='https://github.com/nmiculinic/minion-data',
     packages=['minion_data'],
     include_package_data=True,
     install_requires=install_requires,
-    author = 'Neven Miculinic',
-    author_email = 'neven.miculinic@gmail.com',
-    # ext_modules=cythonize("fino/arbitrage.pyx"),
+    author='Neven Miculinic',
+    author_email='neven.miculinic@gmail.com',
+    # ext_modules=cythonize("example.pyx"),
     # include_dirs=[np.get_include()],
 )
