fb91e3aff1e0bae51aa29e6ae1e67afdbe929dc1
Neven Miculinic <neven.miculinic@gmail.com>
2018-04-29 16:14:56 +0200

    yapf code

--- minion_data/__main__.py
+++ minion_data/__main__.py
@@ -2,7 +2,7 @@ import argparse
 import time
 import logging
 import gzip
-from itertools  import repeat
+from itertools import repeat
 from os import path
 import typing
 import os
@@ -16,12 +16,14 @@ import sys
 import multiprocessing as mp
 import threading
 
+
 class MinionDataCfg(NamedTuple):
     chiron_out: str
     sam_file: str
     out: str
 
-def debug_output(pb: dataset_pb2.DataPoint, screen_width:int=120):
+
+def debug_output(pb: dataset_pb2.DataPoint, screen_width: int = 120):
     ttbl = {
         dataset_pb2.MATCH: "=",
         dataset_pb2.MISMATCH: "X",
@@ -31,28 +33,37 @@ def debug_output(pb: dataset_pb2.DataPoint, screen_width:int=120):
 
     def ff(x):
         if x == dataset_pb2.BLANK:
-           return "-"
+            return "-"
         return dataset_pb2.BasePair.Name(x)
 
     for i in range(0, len(pb.cigar), screen_width):
-        print("CIGAR:", "".join([ttbl[x] for x in pb.cigar[i:i + screen_width]]))
-        print("BaseC:", "".join([ff(x) for x in pb.basecalled_squiggle[i:i + screen_width]]))
-        print("REF  :", "".join([ff(x) for x in pb.aligned_ref_squiggle[i:i + screen_width]]))
+        print(
+            "CIGAR:", "".join([ttbl[x] for x in pb.cigar[i:i + screen_width]])
+        )
+        print(
+            "BaseC:",
+            "".join([ff(x) for x in pb.basecalled_squiggle[i:i + screen_width]])
+        )
+        print(
+            "REF  :", "".join([
+                ff(x) for x in pb.aligned_ref_squiggle[i:i + screen_width]
+            ])
+        )
         print("------", "-" * screen_width)
+
 
 class ProcessDataPointCfg(NamedTuple):
     pt: typing.Tuple[str, typing.Tuple]
     cfg: MinionDataCfg
     completed: mp.Queue
 
+
 def processDataPoint(cfgDp: ProcessDataPointCfg):
     cfg = cfgDp.cfg
     name, v = cfgDp.pt
     ref, _, _, _, cigar_str = v  # target & cigar_str are important
     ref = [dataset_pb2.BasePair.Value(x) for x in ref.upper()]
-    sol = dataset_pb2.DataPoint(
-        aligned_ref=ref,
-    )
+    sol = dataset_pb2.DataPoint(aligned_ref=ref,)
     with open(path.join(cfg.chiron_out, "raw", name + ".signal"), "r") as f:
         signal = np.array(f.readlines()[0].split(), dtype=np.float)
         sol.MergeFrom(dataset_pb2.DataPoint(signal=signal))
@@ -63,10 +74,12 @@ def processDataPoint(cfgDp: ProcessDataPointCfg):
         for l, _, b in [x.split() for x in f.readlines() if len(x)]:
             lower_bound.append(np.int(l))
             bcall.append(dataset_pb2.BasePair.Value(b))
-        sol.MergeFrom(dataset_pb2.DataPoint(
-            basecalled=bcall,
-            lower_bound=lower_bound,
-        ))
+        sol.MergeFrom(
+            dataset_pb2.DataPoint(
+                basecalled=bcall,
+                lower_bound=lower_bound,
+            )
+        )
 
     ref_idx = 0
     bcall_idx = 0
@@ -77,21 +90,24 @@ def processDataPoint(cfgDp: ProcessDataPointCfg):
 
     for cigar in cigar_str:
         if cigar in bioinf_utils.CIGAR_MATCH_MISSMATCH:
-            cigar_lst.append(dataset_pb2.MATCH if cigar in bioinf_utils.CIGAR_MATCH else dataset_pb2.MISMATCH)
+            cigar_lst.append(
+                dataset_pb2.MATCH
+                if cigar in bioinf_utils.CIGAR_MATCH else dataset_pb2.MISMATCH
+            )
             aligned_ref_squiggle.append(ref[ref_idx])
             basecalled_squiggle.append(bcall[bcall_idx])
-            ref_idx+=1
-            bcall_idx+=1
+            ref_idx += 1
+            bcall_idx += 1
         elif cigar in bioinf_utils.CIGAR_INSERTION:
             cigar_lst.append(dataset_pb2.INSERTION)
             basecalled_squiggle.append(bcall[bcall_idx])
             aligned_ref_squiggle.append(dataset_pb2.BLANK)
-            bcall_idx+=1
+            bcall_idx += 1
         elif cigar in bioinf_utils.CIGAR_DELETION:
             cigar_lst.append(dataset_pb2.DELETION)
             basecalled_squiggle.append(dataset_pb2.BLANK)
             aligned_ref_squiggle.append(ref[ref_idx])
-            ref_idx+=1
+            ref_idx += 1
         else:
             raise ValueError(f"Not sure what to do with {cigar}")
 
@@ -100,11 +116,13 @@ def processDataPoint(cfgDp: ProcessDataPointCfg):
     assert ref_idx == len(ref)
     assert bcall_idx == len(bcall)
 
-    sol.MergeFrom(dataset_pb2.DataPoint(
-        cigar=cigar_lst,
-        aligned_ref_squiggle=aligned_ref_squiggle,
-        basecalled_squiggle=basecalled_squiggle,
-    ))
+    sol.MergeFrom(
+        dataset_pb2.DataPoint(
+            cigar=cigar_lst,
+            aligned_ref_squiggle=aligned_ref_squiggle,
+            basecalled_squiggle=basecalled_squiggle,
+        )
+    )
     with gzip.open(path.join(cfg.out, name + ".datapoint"), "w") as f:
         sol_pb_str = sol.SerializeToString()
         f.write(sol_pb_str)
@@ -124,15 +142,17 @@ def main(cfg: MinionDataCfg):
             def f():
                 for _ in range(len(all)):
                     q.get()
-                    pbar.update(1)
+                    pbar.update()
+
             threading.Thread(target=f, daemon=True).start()
-            p.map(processDataPoint, [
-                ProcessDataPointCfg(
+            p.map(
+                processDataPoint,
+                [ProcessDataPointCfg(
                     pt=x,
                     cfg=cfg,
                     completed=q,
-                ) for x in all
-            ])
+                ) for x in all]
+            )
 
 
 if __name__ == "__main__":
@@ -150,9 +170,7 @@ if __name__ == "__main__":
         args.sam_file = args.sam_file or path.join(
             args.default_root, "alignment.sam"
         )
-        args.out = args.out or path.join(
-            args.default_root, "dataset"
-        )
+        args.out = args.out or path.join(args.default_root, "dataset")
     if args.chiron_out is None or args.sam_file is None:
         print(
             "Both chiron_out and sam_file must be defined (( or default_root ))"
