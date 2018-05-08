0afdc4fb04e8fc959d386c7abaab93373ed8246d
Neven Miculinic <neven.miculinic@gmail.com>
2018-05-08 15:30:05 +0200

    Added necessary deployment files

--- setup.py
+++ setup.py
@@ -15,14 +15,33 @@ install_requires = [
 
 setup(
     name="minion_data",
-    version='0.1',
+    version='0.1.1',
     description='',
     url='https://github.com/nmiculinic/minion-data',
-    packages=['minion_data'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
     author='Neven Miculinic',
     author_email='neven.miculinic@gmail.com',
-    # ext_modules=cythonize("example.pyx"),
-    # include_dirs=[np.get_include()],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+
+    ],
+
+    project_urls={  # Optional
+        'Source': 'https://github.com/nmiculinic/minion-data/',
+    },
+
 )
